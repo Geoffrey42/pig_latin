605e555551c91b720821f40ed2dcfbdff3178e9d
Geoffrey Gane <geoffreygane@gmail.com>
2019-11-12 01:41:13 +0100

    test(bin): add test for consonant words

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,11 @@ fn main() {
     let apple = String::from("apple");
 
     let converted_apple = convert::to_pig_latin(&apple);
-    println!("pig latin first: {}", converted_apple);
+    println!("pig latin apple: {}", converted_apple);
+
+    let first = String::from("first");
+
+    let converted_first = convert::to_pig_latin(&first);
+    println!("pig latin first: {}", converted_first);
+
 }
