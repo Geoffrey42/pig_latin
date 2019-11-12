792eb1db402a59abc142a2b944f7e2c444fd508b
Geoffrey Gane <geoffreygane@gmail.com>
2019-11-12 01:29:29 +0100

    feat(lib): handle voyel first letter words

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+pub mod convert {
+
+    use std::collections::HashMap;
+
+    pub fn to_pig_latin(original: &String) -> String {
+        let first_letter = original.chars().next();
+        let mut map = HashMap::new();
+
+        map.insert('A', 'a');
+        map.insert('E', 'e');
+        map.insert('I', 'i');
+        map.insert('O', 'o');
+        map.insert('U', 'u');
+        map.insert('Y', 'y');
+
+        for (upper, lower) in map {
+            if first_letter == Some(upper) || first_letter == Some(lower) {
+                let converted = format!("{}-fay", original);
+                return converted;
+            }
+        }
+        original.to_string()
+    }
+}
